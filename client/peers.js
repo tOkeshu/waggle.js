c3c5afc7bffc8b3b5c7ca075771e13988c4dac29
Romain Gauthier <romain.gauthier@monkeypatch.me>
2014-07-29 14:32:33 +0200

    Implement Peers and Peer objects

---
+++ client/peers.js
@@ -0,0 +1,195 @@
+var Peers = (function() {
+  var encoder = new tnetbin.Encoder({arraybuffer: true});
+  var decoder = new tnetbin.Decoder();
+  var blobDecoder = new tnetbin.Decoder({arraybuffer: true});
+
+  function Peer(id, config) {
+    var pc = new RTCPeerConnection({
+      iceServers: [{
+        // please contact me if you plan to use this server
+        url: 'turn:webrtc.monkeypatch.me:6424?transport=udp',
+        credential: 'hibuddy',
+        username: 'hibuddy'
+      }]
+    });
+    var dc;
+
+    pc.oniceconnectionstatechange = this._onIceStateChange.bind(this);
+    pc.onicecandidate = function(event) {
+      this.trigger("icecandidate", event);
+    }.bind(this);
+    dc = pc.createDataChannel("waggle", {id: 0, negotiated: true});
+    dc.onopen = this._onDatachannelOpen.bind(this);
+    dc.onmessage = this._onMessage.bind(this);
+    dc.binaryType = "arraybuffer";
+
+    this.id = id;
+    this.pc = pc;
+    this.dc = dc;
+    this.queue = [];
+  }
+
+  Peer.prototype = {
+    request: function(chunk) {
+      console.log("request chunk #" + chunk.id + " from " + this.id);
+
+      this._send({
+        type: "request",
+        swarmId: chunk.swarm,
+        chunkId: chunk.id
+      });
+    },
+
+    send: function(chunk) {
+      this._send({
+        type: "chunk",
+        swarmId: chunk.swarm,
+        chunkId: chunk.id
+      }, [chunk.data]);
+    },
+
+    createOffer: function(callback) {
+      console.log("createoffer");
+      this.pc.createOffer(function(offer) {
+        this.pc.setLocalDescription(offer, function() {
+          callback(offer);
+        });
+      }.bind(this), function() {});
+    },
+
+    createAnswer: function(offer, callback) {
+      offer = new RTCSessionDescription(offer);
+      this.pc.setRemoteDescription(offer, function() {
+        this.pc.createAnswer(function(answer) {
+          this.pc.setLocalDescription(answer, function() {
+            callback(answer)
+          });
+        }.bind(this), function() {});
+      }.bind(this), function() {});
+    },
+
+    complete: function(answer, callback) {
+      answer = new RTCSessionDescription(answer);
+      this.pc.setRemoteDescription(answer, callback);
+    },
+
+    addIceCandidate: function(candidate) {
+      candidate = new RTCIceCandidate(candidate);
+      this.pc.addIceCandidate(candidate);
+    },
+
+    isConnected: function() {
+      var iceState = this.pc.iceConnectionState;
+      if (iceState === "connected" ||
+          iceState === "completed")
+        return true;
+
+      return false;
+    },
+
+    willConnect: function() {
+      var iceState = this.pc.iceConnectionState;
+      if (iceState === "new" ||
+          iceState === "checking")
+        return true;
+
+      return false;
+    },
+
+    _onIceStateChange: function() {
+      // XXX: display an error if the ice connection failed
+      console.log("ice: " + this.pc.iceConnectionState);
+      if (this.pc.iceConnectionState === "failed") {
+        console.error("Something went wrong: the connection failed");
+        this.trigger("failure");
+      }
+
+      if (this.pc.iceConnectionState === "connected")
+        this.trigger("connected");
+    },
+
+    _send: function(message, blobs) {
+      if (this.dc.readyState === "connecting") {
+        this.queue.push([message, blobs]);
+        return;
+      }
+
+      message = encoder.encode(message);
+      blobs = blobs ? encoder.encode(blobs) : new Uint8Array(0);
+      // TODO: handle errors when the datachannel is closed
+      this.dc.send(tnetbin.concatArrayBuffers([message, blobs]));
+    },
+
+    _onDatachannelOpen: function() {
+      var item, message, blobs;
+      while (this.queue.length > 0) {
+        item = this.queue.shift();
+        message = item[0];
+        blobs = item[1];
+        this._send(message, blobs);
+      }
+    },
+
+    _onMessage: function(event) {
+      var payload, message, remain;
+
+      payload = decoder.decode(event.data);
+      message = payload.value;
+      remain = payload.remain;
+
+      if (remain) {
+        payload = blobDecoder.decode(remain);
+        message.blobs = payload.value;
+      } else
+        message.blobs = []
+
+      this.trigger(message.type, message);
+    }
+  };
+
+  MicroEvent.mixin(Peer);
+  Peer.prototype.on = Peer.prototype.bind;
+
+
+  function Peers(config) {
+    this.peers = {};
+  }
+
+  Peers.prototype = {
+    get: function(id) {
+      return this.peers[id];
+    },
+
+    add: function(id) {
+      var peer = new Peer(id);
+      this.peers[id] = peer;
+      this.trigger("add", peer);
+      return peer;
+    },
+
+    isConnected: function(id) {
+      var peer = this.peers[id];
+      if (!peer)
+        return false;
+
+      var iceState = peer.pc.iceConnectionState;
+      if (iceState === "failed" ||
+          iceState === "disconnected" ||
+          iceState === "closed")
+        return false;
+
+      return peer.pc.iceConnectionState === "connected";
+    },
+
+    forEach: function(callback) {
+      Object.keys(this.peers).forEach(function(id) {
+        callback(this.peers[id]);
+      }.bind(this));
+    }
+  };
+
+  MicroEvent.mixin(Peers);
+  Peers.prototype.on = Peers.prototype.bind;
+
+  return Peers;
+}());
---
+++ client/tnetbin.js
@@ -0,0 +1,341 @@
+var tnetbin = (function() {
+
+  var COLON   = 58;
+  var ZERO    = 48;
+  var NULL    = 126;
+  var BOOLEAN = 33;
+  var INTEGER = 35;
+  var FLOAT   = 94;
+  var STRING  = 44;
+  var LIST    = 93;
+  var DICT    = 125;
+
+  function Encoder(options) {
+    this.options = options || {};
+  }
+
+  Encoder.prototype = {
+    encode: function(obj) {
+      if (this.options.arraybuffer)
+        return this._encodeToArrayBuffer(obj);
+
+      return this._encodeToString(obj);
+    },
+
+    _encodeToArrayBuffer: function(obj) {
+      switch (obj) {
+      case null:
+        return new Uint8Array([48, 58, 126]); // '0:~'
+      case true:
+        return new Uint8Array([52, 58, 116, 114, 117, 101, 33]); // '4:true!'
+      case false:
+         // '5:false!'
+        return new Uint8Array([53, 58, 102, 97, 108, 115, 101, 33]);
+      }
+
+      var type = typeof obj, s, tag;
+
+      switch (type) {
+      case 'string':
+        s   = toArrayBuffer(obj);
+        tag = new Uint8Array([STRING]);
+        break;
+      case 'number':
+        s = toArrayBuffer(obj.toString());
+        // Integer
+        if (obj % 1 === 0)
+          tag = new Uint8Array([INTEGER]); // '#'
+        // Float
+        else
+          tag = new Uint8Array([FLOAT]); // '^'
+        break;
+      case 'object':
+        if (obj instanceof ArrayBuffer) { // ArrayBuffer
+          s = new Uint8Array(obj);
+          tag = new Uint8Array([STRING]);
+        } else if (obj instanceof Int8Array    || // ArrayBufferView
+                   obj instanceof Uint8Array   ||
+                   obj instanceof Uint16Array  ||
+                   obj instanceof Int32Array   ||
+                   obj instanceof Uint32Array  ||
+                   obj instanceof Float32Array ||
+                   obj instanceof Float64Array) {
+          s = new Uint8Array(obj.buffer);
+          tag = new Uint8Array([STRING]);
+        }else if (obj instanceof Array) { // List
+          s = obj.map(this._encodeToArrayBuffer.bind(this));
+          s = concatArrayBuffers(s);
+          tag = new Uint8Array([LIST]);
+        } else { // Object
+          var attrs = [];
+          for (var attr in obj) {
+            if (obj.hasOwnProperty(attr)) {
+              attrs.push(this._encodeToArrayBuffer(attr),
+                         this._encodeToArrayBuffer(obj[attr]));
+            }
+          }
+          s = concatArrayBuffers(attrs);
+          tag = new Uint8Array([DICT]);
+        }
+      }
+
+      var size = toArrayBuffer(s.byteLength.toString());
+      return concatArrayBuffers([size, COLON, s, tag])
+    },
+
+    _encodeToString: function(obj) {
+      switch (obj) {
+      case null:
+        return '0:~';
+      case true:
+        return '4:true!';
+      case false:
+        return '5:false!';
+      }
+
+      var type = typeof obj, s, tag;
+
+      switch (type) {
+      case 'string':
+        s   = obj;
+        tag = ',';
+        break;
+      case 'number':
+        s = obj.toString();
+        // Integer
+        if (obj % 1 === 0)
+          tag = '#';
+        // Float
+        else
+          tag = '^';
+        break;
+      case 'object':
+        if (obj instanceof ArrayBuffer) { // ArrayBuffer
+          s = largeArrayToString(obj);
+          tag = ',';
+        } else if (obj instanceof Int8Array    || // ArrayBufferView
+                   obj instanceof Uint8Array   ||
+                   obj instanceof Uint16Array  ||
+                   obj instanceof Int32Array   ||
+                   obj instanceof Uint32Array  ||
+                   obj instanceof Float32Array ||
+                   obj instanceof Float64Array) {
+          s = largeArrayToString(obj.buffer);
+          tag = ',';
+        } else if (obj instanceof Array) { // List
+          s = obj.map(this.encode.bind(this)).join('');
+          tag = ']';
+        } else { // Object
+          var attrs = [];
+          for (var attr in obj) {
+            if (obj.hasOwnProperty(attr)) {
+              attrs.push(this.encode(attr),
+                         this.encode(obj[attr]));
+            }
+          }
+          s = attrs.join('');
+          tag = '}';
+        }
+      }
+
+      return s.length + ':' + s + tag;
+    }
+  };
+
+  function Decoder(options) {
+    this.options = options || {};
+  }
+
+  Decoder.prototype = {
+    decode: function(data) {
+      var result;
+      data = toArrayBuffer(data);
+      result = this._decode(data, 0);
+
+      return {value: result.value, remain: this.remain(data, result.cursor)};
+    },
+
+    _decode: function(data, cursor) {
+      var result = this._decodeSize(data, cursor);
+      return this._decodePayload(data, result.cursor, result.value);
+    },
+
+    _decodeSize: function(data, cursor, callback) {
+      for (var size=0; data[cursor] !== COLON; cursor++) {
+        size = size*10 + (data[cursor] - ZERO);
+      }
+
+      return {value: size, cursor: cursor + 1};
+    },
+
+    _decodePayload: function(data, cursor, size) {
+      var tag = data[cursor + size];
+      switch (tag) {
+      case NULL:
+        return this._decodeNull(data, cursor, size);
+      case BOOLEAN:
+        return this._decodeBoolean(data, cursor, size);
+      case INTEGER:
+        return this._decodeInteger(data, cursor, size);
+      case FLOAT:
+        return this._decodeFloat(data, cursor, size);
+      case STRING:
+        return this._decodeString(data, cursor, size);
+      case LIST:
+        return this._decodeList(data, cursor, size);
+      case DICT:
+        return this._decodeDict(data, cursor, size);
+      default:
+        return {value: undefined, cursor: cursor + size + 1};
+      }
+    },
+
+    remain: function(data, cursor) {
+      var d = data.subarray(cursor);
+      return (this.options.arraybuffer ? d : largeArrayToString(d));
+    },
+
+    _decodeNull: function(data, cursor) {
+      return {value: null, cursor: cursor + 1};
+    },
+
+    _decodeBoolean: function(data, cursor, size) {
+      return {value: (size === 4) ? true : false, cursor: cursor + size + 1};
+    },
+
+    _decodeInteger: function(data, cursor, size) {
+      var end = cursor + size;
+      for (var value = 0; cursor < end; cursor++)
+        value = value*10 + (data[cursor] - ZERO);
+
+      return {value: value, cursor: cursor + 1};
+    },
+
+    _decodeFloat: function(data, cursor, size) {
+      var value, exp, decimal;
+      var end = cursor + size;
+
+      for(value = 0; data[cursor] !== 46; cursor++)
+        value = value*10 + (data[cursor] - ZERO);
+
+      cursor++;
+      for (decimal = 0, exp=1; cursor < end; cursor++, exp*=10) {
+        decimal = decimal*10 + (data[cursor] - ZERO);
+      }
+
+      return {value: value + (decimal/exp), cursor: cursor + 1};
+    },
+
+    _decodeString: function(data, cursor, size) {
+      var s;
+      data = data.subarray(cursor, cursor + size);
+
+      if (this.options.arraybuffer)
+        return {value: data, cursor: cursor + size + 1};
+      else
+        return {value: largeArrayToString(data), cursor: cursor + size + 1};
+    },
+
+    _decodeList: function(data, cursor, size) {
+      if (size === 0)
+        return {value: [], cursor: cursor + 1};
+
+      var list = [];
+      var end  = cursor + size;
+      var result;
+
+      do {
+        result = this._decode(data, cursor);
+        list.push(result.value);
+        cursor = result.cursor;
+      } while (cursor < end);
+
+      return {value: list, cursor: result.cursor + 1};
+    },
+
+    _decodeDict: function(data, cursor, size) {
+      if (size === 0)
+        return {value: {}, cursor: cursor + 1};
+
+      var dict = {};
+
+      var result = this._decodeList(data, cursor, size);
+      var items  = result.value;
+      var len    = items.length;
+      var key;
+
+      for (var i = 0; i < len; i+=2) {
+        key = items[i];
+        key = this.options.arraybuffer ? largeArrayToString(key) : key;
+        dict[key] = items[i + 1];
+      }
+
+      return {value: dict, cursor: result.cursor};
+    }
+  };
+
+  function isAString(data) {
+    if (data.byteLength % 2 !== 0)
+      return false;
+
+    data = new Uint16Array(data.buffer);
+    for (var i=0; i < data.byteLength; i++)
+      if (0x000 <= data[i] && data[i] <= 0x00)
+        return false
+    return true;
+  }
+
+  function toArrayBuffer(data) {
+    if (data instanceof ArrayBuffer)
+      return new Uint8Array(data);
+
+    var len  = data.length;
+    var view = new Uint8Array(len);
+    // Transform the string to an array buffer
+    for (var cursor = 0; cursor < len; cursor++)
+      view[cursor] = data.charCodeAt(cursor);
+
+    return view;
+  }
+
+  function splitArrayBuffer(data) {
+    var len = data.byteLength;
+    var arrays = [];
+    for (var i = 0; (i*2048) < len; i++)
+      arrays.push(data.subarray((i * 2048), (i * 2048) + 2048));
+    return arrays;
+  }
+
+  function largeArrayToString(data) {
+    var s = '';
+    splitArrayBuffer(new Uint8Array(data)).forEach(function (array) {
+      s += String.fromCharCode.apply(null, array);
+    });
+    return s;
+  }
+
+  function concatArrayBuffers(arrays) {
+    var byteLength = arrays.reduce(function(acc, b) {
+      return (typeof b === 'number') ? acc + 1 : acc + b.byteLength;
+    }, 0);
+    var result = new Uint8Array(byteLength);
+    var offset = 0;
+
+    arrays.forEach(function(array) {
+      array = (typeof array === 'number') ? new Uint8Array([array]) : array;
+      result.set(array, offset);
+      offset += array.byteLength;
+    });
+
+    return result;
+  }
+
+  return {
+    Encoder: Encoder,
+    Decoder: Decoder,
+    isAString: isAString,
+    toArrayBuffer: toArrayBuffer,
+    concatArrayBuffers: concatArrayBuffers
+  }
+}());
+
