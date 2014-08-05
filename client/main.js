41d3ef3c7d4d96304bcbfb4a2fd483342434beb2
Romain Gauthier <romain.gauthier@monkeypatch.me>
2014-08-05 11:35:44 +0200

    Display basic warnings for flags and non Firefox browsers

--- client/main.js
+++ client/main.js
@@ -1,7 +1,29 @@
 (function() {
   var video = document.querySelector("video");
   var peers = document.querySelector(".peers-connected span");
-  var waggler = new Waggler("foo");
+  var alert = document.querySelector(".alert");
+  var waggler;
+
+  if (!window.mozRTCPeerConnection) {
+    alert.textContent = [
+      "This demo only works with Firefox for now. ",
+      "Any contribution to support other browsers would be greatly appreciated"
+    ].join('');
+    alert.classList.remove("hide");
+    return;
+  }
+
+  if (!window.MediaSource) {
+    alert.textContent = [
+      "Your browser does not support MediaSource. ",
+      "On Mozilla Firefox you can switch media.mediasource.enabled ",
+      "to true in about:config"
+    ].join('');
+    alert.classList.remove("hide");
+    return;
+  }
+
+  waggler = new Waggler("foo");
 
   waggler.source.on("uid", function() {
     waggler.listenFor(video.dataset.src, video);
