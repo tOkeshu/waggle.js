8555b608b8cb1e1625964f280d35ff6a44a68505
Romain Gauthier <romain.gauthier@monkeypatch.me>
2014-08-10 13:54:24 +0200

    Implement the Video class
    Video#buffer is here to help with the buffering of chunks

--- client/waggle.js
+++ client/waggle.js
@@ -176,21 +176,61 @@ var Waggler = (function() {
     }
   };
 
+  function Video(el) {
+    this.mediaSource = new MediaSource();
+    this.queue = [];
+    this.el = el;
+    this._lastChunkSeen = null;
+
+    this.mediaSource.addEventListener('sourceopen', function() {
+      this.mediaSource.addSourceBuffer('video/webm; codecs="vorbis,vp8"');
+    }.bind(this));
+
+    this.el.src = URL.createObjectURL(this.mediaSource);
+  }
+
+  Video.prototype = {
+    buffer: function(chunk) {
+      // First chunk
+      if  (this._lastChunkSeen === null) {
+        this.mediaSource.sourceBuffers[0].appendBuffer(chunk.data);
+        this._lastChunkSeen = chunk;
+        return;
+      }
+
+      if (chunk.id <= this._lastChunkSeen.id)
+        throw new Error("WTF");
+
+      if (chunk.id > this._lastChunkSeen.id + 1) {
+        this.queue.push(chunk);
+        return;
+      }
+
+      this.queue.reduce(function(acc, chunk) {
+        var lastChunk = acc[acc.length - 1];
+
+        if ((lastChunk.id + 1) === (chunk.id))
+          acc.push(chunk);
+
+        return acc;
+      }, [chunk]).forEach(function(chunk) {
+        this.mediaSource.sourceBuffers[0].appendBuffer(chunk.data);
+        this._lastChunkSeen = chunk;
+      }.bind(this));
+
+      this.queue.splice(this.queue.indexOf(this._lastChunkSeen));
+    }
+  }
+
   function Videos() {
     this.videos = {};
   }
 
   Videos.prototype = {
     add: function(fileId, el) {
-      var mediaSource = new MediaSource();
-      el.src = URL.createObjectURL(mediaSource);
-
-      mediaSource.addEventListener('sourceopen', function() {
-        mediaSource.addSourceBuffer('video/webm; codecs="vorbis,vp8"');
-        this.videos[fileId] = mediaSource;
-      }.bind(this));
-
-      return el;
+      var video = new Video(el);
+      this.videos[fileId] = video;
+      return video;
     },
 
     get: function(fileId) {
@@ -306,14 +346,12 @@ var Waggler = (function() {
 
     _setupSwarm: function(swarm) {
       swarm.on("chunk", function(chunk) {
-        var mediaSource = this.videos.get(swarm.id);
-        mediaSource.sourceBuffers[0].appendBuffer(chunk.data);
+        var video = this.videos.get(swarm.id);
         this._post('/api/rooms/' + this.room + '/files/' + swarm.id + '/index', {
           chunk: chunk.id
         });
-        setTimeout(function() {
-          swarm.want(chunk.id + 1);
-        }, 0);
+
+        video.buffer(chunk);
       }.bind(this));
 
       swarm.on("chunk:wanted", function(chunk) {
